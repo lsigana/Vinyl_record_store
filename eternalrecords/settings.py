82d37d030a23bd1f55205d6502f4a4fc845404b9
Leah Sigana <160542207+arieltesfaye@users.noreply.github.com>
2025-02-02 19:38:15 +0300

    Fixed static files settings and added missing dependencies

--- eternalrecords/settings.py
+++ eternalrecords/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5zv#zof+))hrh$8wdv_8f7qetczkn%oj6=q(c1lrc9ckvpus)q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG=False
+SECRET_KEY=your-secret-key
+ALLOWED_HOSTS=[os.environ.get("ALLOWED_HOSTS", "your-railway-app-url")]
+DATABASE_URL=your_postgresql_url
 
 
 # Application definition
@@ -84,6 +87,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        "default": dj_database_url.config(default=os.environ.get("DATABASE_URL")),
     }
 }
 
@@ -121,10 +125,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'store', 'static'),
+    os.path.join(BASE_DIR, 'static', 'store'),
 ]
 
 MEDIA_URL = '/media/'
