ae1bc9ef9441de5c6418b142dda23f7fc54938da
Leah Sigana <160542207+arieltesfaye@users.noreply.github.com>
2025-02-06 18:34:34 +0300

    Fixed SECRET_KEY issue

--- eternalrecords/settings.py
+++ eternalrecords/settings.py
@@ -88,6 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
         "default": dj_database_url.config(default=os.environ.get("DATABASE_URL")),
+        
     }
 }
 
